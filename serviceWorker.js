8327a557c1f15ca1063c6aeb9f95bcf1b81acb6c
Archianne <helena19w@gmail.com>
2021-08-24 18:21:14 +0100

    Updates

--- serviceWorker.js
+++ serviceWorker.js
@@ -1,3 +1,4 @@
+/* eslint-disable-next-line no-restricted-globals */
 self.addEventListener("install", (e) => {
   e.waitUntil(
     caches.open("static").then((cache) => {
@@ -7,7 +8,7 @@ self.addEventListener("install", (e) => {
     })
   );
 });
-
+/* eslint-disable-next-line no-restricted-globals */
 self.addEventListener("fetch", (e) => {
   e.respondWith(
     caches.match(e.request).then((response) => {
