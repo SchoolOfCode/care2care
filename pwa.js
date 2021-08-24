038ed08509c3389f69d0db332d879b2509d906bc
Archianne <helena19w@gmail.com>
2021-08-24 17:34:46 +0100

    Updates

--- pwa.js
+++
@@ -1,9 +0,0 @@
-if (navigator.serviceWorker) {
-  navigator.serviceWorker
-    .register('/care2care/serviceWorker.js', {scope: '/care2care/#/'})
-    .then(registration => {
-        console.log("service worker registered");
-    }).catch(error => {
-        console.log("service worker not registered", error);
-})
-}
