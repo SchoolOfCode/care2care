483287fe8d2ab73280471ad7e31a01ee0780c91b
Archianne <helena19w@gmail.com>
2021-08-25 14:14:29 +0100

    auth0 locks application if user is not logged in

--- src/App.jsx
+++ src/App.jsx
@@ -7,6 +7,26 @@ import useLocalStorage from "./components/__Hooks/useLocalStorage";
 import NavBar from "./components/NavBar.js";
 import Main from "./components/Main.js";
 import { useAuth0 } from "@auth0/auth0-react";
+import LoginButton from "./components/LoginButton";
+
+const Authenticated = (props) => {
+  return (
+    <>
+      <NavBar />
+      <Main />
+    </>
+  );
+};
+
+const NotAuthenticated = (props) => {
+  return <LoginButton />;
+};
+
+const FirstPage = (props) => {
+  const { isAuthenticated } = useAuth0();
+
+  return isAuthenticated ? <Authenticated /> : <NotAuthenticated />;
+};
 
 const App = () => {
   const [theme, setTheme] = useLocalStorage("theme", "dark");
@@ -14,14 +34,12 @@ const App = () => {
     theme === "dark" ? setTheme("light") : setTheme("dark");
   };
   const { isLoading } = useAuth0();
-
   if (isLoading) return <div>Loading...</div>;
 
   return (
     <ThemeProvider theme={themes[theme]}>
       <GlobalStyle changeTheme={changeTheme} />
-      <NavBar />
-      <Main />
+      <FirstPage />
     </ThemeProvider>
   );
 };
