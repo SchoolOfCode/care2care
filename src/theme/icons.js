98664f66b86e0a97b03a4b121467bfc6461beec9
Archianne <helena19w@gmail.com>
2021-08-25 16:45:42 +0100

    icons

---
+++ src/theme/icons.js
@@ -0,0 +1,18 @@
+import { IoLogIn, IoLogOut, IoSettings } from "react-icons/io";
+import { RiUserHeartFill } from "react-icons/ri";
+import { FaUserAlt } from "react-icons/fa";
+import { BiMessageAltAdd, BiMessageAltCheck } from "react-icons/bi";
+
+const Icons = {
+  Login: IoLogIn,
+  Logout: IoLogOut,
+
+  //NavBar
+  Patients: RiUserHeartFill,
+  AddRecord: BiMessageAltAdd,
+  Profile: FaUserAlt,
+  CheckRecords: BiMessageAltCheck,
+  Settings: IoSettings,
+};
+
+export default Icons;
