dd24fc9ebda2b1b657faf186de960c2eaeb8c486
Archianne <helena19w@gmail.com>
2021-08-30 19:28:46 +0100

    Updates

--- static/js/3.f5472dfe.chunk.js
+++
@@ -1,2 +0,0 @@
-(this.webpackJsonpcare2care=this.webpackJsonpcare2care||[]).push([[3],{55:function(c,e,r){"use strict";r.r(e);var t=r(2);e.default=function(){return Object(t.jsx)("h1",{children:"PATIENT PROFILE"})}}}]);
-//# sourceMappingURL=3.f5472dfe.chunk.js.map
