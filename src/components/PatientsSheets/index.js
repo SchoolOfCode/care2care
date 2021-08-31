86dcbc555b8439591adeb28363b550e214be0d96
Archianne <helena19w@gmail.com>
2021-08-31 13:33:54 +0100

    .

--- src/components/PatientsSheets/index.js
+++ src/components/PatientsSheets/index.js
@@ -1,7 +1,7 @@
 import React, { useEffect, useState } from "react";
 import Papa from "papaparse";
 import styled from "styled-components";
-import JSONPretty from "react-json-pretty";
+// import JSONPretty from "react-json-pretty";
 
 const URL =
   "https://docs.google.com/spreadsheets/d/e/2PACX-1vQk2shcRCfjZcMqWRoT8BXaIymooGq2pcd_heGDVrzQYnT0RL6hxHAq8M6FF9kZm5mILBmjg1qPQJc6/pub?output=csv";
@@ -53,7 +53,7 @@ const PatientSheets = () => {
             <p>Address: {item.Address}</p>
             <p>Comments: {item.Comments}</p>
           </div>
-          <JSONPretty data={item} />
+          {/* <JSONPretty data={item} /> */}
         </ul>
       );
     });
