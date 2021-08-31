40ee78bd9ad3c2376ff2b0a1c39389b3196aa6ee
Archianne <helena19w@gmail.com>
2021-08-31 17:46:13 +0100

    colours set

--- src/components/StyledButton/index.js
+++ src/components/StyledButton/index.js
@@ -2,11 +2,11 @@ import styled from "styled-components";
 
 export const StyledButton = styled.button`
   height: 40px;
-  width: 100px;
+  width: 150px;
   border: none;
   border-radius: 10px;
   color: #f5f5f5;
-  background-color: ${(props) => props.theme.accent1};
+  background-color: ${(props) => props.theme.accent2};
   font-weight: bold;
   cursor: pointer;
   box-shadow: rgba(0, 0, 0, 0.15) 1.95px 1.95px 2.6px;
--- src/theme/theme.js
+++ src/theme/theme.js
@@ -3,7 +3,7 @@ const LightTheme = {
   fontColor1: "#f5f5f5",
   fontColor2: "#333333",
   accent1: "#49C6E5",
-  accent2: "#00BD9D",
+  accent2: "#51ADE2",
   notActive: "#aaaaaa"
 };
 
@@ -11,9 +11,9 @@ const DarkTheme = {
   bg: "#2D3F50",
   fontColor1: "#333333",
   fontColor2: "#f5f5f5",
-  accent1: "#54DEFD",
+  accent1: "#00BD9D",
   accent2: "#8BD7D2",
-  notActive: "#444444"
+  notActive: "#16202b"
 };
 
 const themes = {
