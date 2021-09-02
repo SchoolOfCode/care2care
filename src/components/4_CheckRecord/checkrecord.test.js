0a5b0075180acc6b2d4254d23cfe9ec5729dbd6f
Yasminamayeh <yasminmayeh@gmail.com>
2021-09-02 21:28:03 +0100

    check records unit test complete

--- src/components/4_CheckRecord/checkrecord.test.js
+++ src/components/4_CheckRecord/checkrecord.test.js
@@ -7,7 +7,7 @@ describe("check record sheets", () => {
   it("this should fetch the users details and render them", async () => {
     //arrange to mock out the gs  2/ render the coponent
     const response = `Timestamp,Patient,Job_Title,Records,Comments,Email
-  27/08/2021 23:31:29,Emilio Comiling,Manager,Wellbeing,"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum",`;
+27/08/2021 23:31:29,Emilio Comiling,Manager,Wellbeing,"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum",`;
 
     nock("https://docs.google.com").get(
       "/spreadsheets/d/e/2PACX-1vRylb8I0x2LH26SwEXfv7HXfN_91VfLiOlEWxFDWp7VDxiS76XdGIqOoM8nNE9Yx2-dtVR0CyXjpRSh/pub?output=csv",
@@ -20,5 +20,20 @@ describe("check record sheets", () => {
         <CheckRecordSheets />
       </UserContext.Provider>
     );
+
+    //ACT
+    // userEvent.click(screen.findByText('button text'))
+
+    //given
+    // when
+    // then
+    //Act
+    // CLICK ON , TYPE
+
+    const name = await screen.findByText("Emilio Comiling");
+
+    expect(name).toBeTruthy();
+    //assert
+    // the state the app should be at (screen what should be displayed)on the text
   });
 });
