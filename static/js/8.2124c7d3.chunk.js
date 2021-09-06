e4b8954818025aae4ee54f25dd5ca0b7cf68d093
Archianne <helena19w@gmail.com>
2021-09-06 12:48:09 +0100

    Updates

--- static/js/8.2124c7d3.chunk.js
+++
@@ -1,2 +0,0 @@
-(this.webpackJsonpcare2care=this.webpackJsonpcare2care||[]).push([[8],{68:function(e,c,t){"use strict";t.r(c);var a,n=t(4),r=t(3),i=t(2);c.default=function(){return Object(i.jsx)(h,{children:Object(i.jsx)("h1",{children:"Add New Patient"})})};var h=r.d.div(a||(a=Object(n.a)(["\n  height: calc(100vh - 140px);\n"])))}}]);
-//# sourceMappingURL=8.2124c7d3.chunk.js.map
