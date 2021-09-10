088657436a690a73b9d0ea911ba343f0fc2e1f9a
EmilioBC <62893436+EmilioBC@users.noreply.github.com>
2021-09-10 02:11:07 +0100

    Something missing

--- src/components/__Pages/6_newPatient.js
+++ src/components/__Pages/6_newPatient.js
@@ -26,6 +26,21 @@ const NewPatient = () => {
     const [history, setHistory] = useState("");
     const [formSubmitted, setFormSubmitted] = useState("");
 
+    useEffect(() => {
+      if (formSubmitted) {
+        setFormSubmitted(false);
+        const { msgSent } = setTimeout(() => setFormSubmitted(""), 4000);
+        return () => {
+          clearTimeout(msgSent);
+        };
+      } else {
+        const { msgSent } = setTimeout(() => setFormSubmitted(""), 4000);
+        return () => {
+          clearTimeout(msgSent);
+        };
+      }
+    }, [formSubmitted]);
+  
 
   return (
     <StyledNewPatient>
@@ -39,12 +54,24 @@ const NewPatient = () => {
           style={{ display: "none" }}
         ></iframe>
 
+        <form
+          action={FORM_ACTION}
+          method="post"
+          target="hidden_iframe"
+          onSubmit={(e) => {
+            
+              e.preventDefault();
+            }
+          }
+        >
+
           <input
             className=""
             type="text"
             name="entry.1864384875"
             value={patientName}
             placeholder={"Name"}
+            onChange={(e) => setPatientName(e.target.value)}
           />
 
           <input
@@ -53,6 +80,7 @@ const NewPatient = () => {
             name="entry.2035833587"
             value={summary}
             placeholder={"Summary"}
+            onChange={(e) => setSummary(e.target.value)}
             />
 
 
@@ -62,6 +90,7 @@ const NewPatient = () => {
             name="entry.1633351042"
             value={email}
             placeholder={"Email"}
+            onChange={(e) => setEmail(e.target.value)}
           />
 
           <input
@@ -70,6 +99,7 @@ const NewPatient = () => {
             name="entry.1523701268"
             value={picture}
             placeholder={"Picture link"}
+            onChange={(e) => setPicture(e.target.value)}
           />
 
           <input
@@ -78,6 +108,7 @@ const NewPatient = () => {
             name="entry.1717570078"
             value={dob}
             placeholder={"Date of Birth"}
+            onChange={(e) => setDob(e.target.value)}
           />
 
           <input
@@ -86,6 +117,7 @@ const NewPatient = () => {
             name="entry.86638701"
             value={diagnosis}
             placeholder={"Diagnosis"}
+            onChange={(e) => setDiagnosis(e.target.value)}
           />
 
           <input
@@ -94,6 +126,7 @@ const NewPatient = () => {
             name="entry.790819135"
             value={allergies}
             placeholder={"Allergies"}
+            onChange={(e) => setAllergies(e.target.value)}
           />
 
           <input
@@ -102,6 +135,7 @@ const NewPatient = () => {
             name="entry.1789297383"
             value={address}
             placeholder={"Address"}
+            onChange={(e) => setAddress(e.target.value)}
           />
           <input
             className=""
@@ -109,6 +143,7 @@ const NewPatient = () => {
             name="entry.365648476"
             value={nhsNumber}
             placeholder={"NHS Number"}
+            onChange={(e) => setNhsNumber(e.target.value)}
           />
 
           <input
@@ -117,6 +152,7 @@ const NewPatient = () => {
             name="entry.1876095816"
             value={gp}
             placeholder={"Registered GP"}
+            onChange={(e) => setGp(e.target.value)}
           />
 
           <input
@@ -125,6 +161,7 @@ const NewPatient = () => {
             name="entry.1252200599"
             value={nextOfKin}
             placeholder={"Next of Kin"}
+            onChange={(e) => setNextOfKin(e.target.value)}
           />
 
           <input
@@ -133,6 +170,7 @@ const NewPatient = () => {
             name="entry.66985754"
             value={nextOfKinContact}
             placeholder={"Next of Kin Contacts"}
+            onChange={(e) => setNextOfKinContact(e.target.value)}
           />
 
           <input
@@ -141,6 +179,7 @@ const NewPatient = () => {
             name="entry.1981488507"
             value={medication}
             placeholder={"Medications"}
+            onChange={(e) => setMedication(e.target.value)}
           />
 
           <input
@@ -149,6 +188,7 @@ const NewPatient = () => {
             name="entry.486109066"
             value={dnr}
             placeholder={"DNR (Do Not Resuscitate"}
+            onChange={(e) => setDnr(e.target.value)}
           />
 
           <input
@@ -157,6 +197,7 @@ const NewPatient = () => {
             name="entry.1942624801"
             value={father}
             placeholder={"Father"}
+            onChange={(e) => setFather(e.target.value)}
           />
 
           <input
@@ -165,6 +206,7 @@ const NewPatient = () => {
             name="entry.888867955"
             value={mother}
             placeholder={"Mother"}
+            onChange={(e) => setMother(e.target.value)}
           />
 
           <inputs
@@ -173,6 +215,7 @@ const NewPatient = () => {
             name="entry.314012053"
             value={history}
             placeholder={"History"}
+            onChange={(e) => setHistory(e.target.value)}
           />
 
           <div className="button">
@@ -180,7 +223,7 @@ const NewPatient = () => {
           </div>
 
           <div className="submitted-status">{formSubmitted}</div>
-
+          </form>
       </StyledForm>
 
     </StyledNewPatient>
